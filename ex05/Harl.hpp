a53cfd2547aa16e1957f33092d15c20c944d0352
Talan <talanlipanda@gmail.com>
2025-02-06 18:50:15 +0100

    Outer Space Bih - Future

---
+++ ex05/Harl.hpp
@@ -0,0 +1,23 @@
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+
+class Harl
+{
+	private:
+		void	debug();
+		void	info();
+		void	warning();
+		void	error();
+
+	public:
+		Harl();
+		~Harl();
+		void    complain(std::string level);
+};
+
+#endif
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -15,37 +15,10 @@
 #include <cstring>
 #include <fstream>
 
-
-void	SedIsForLosers(std::string filename, std::string s1, std::string s2) {
-
-	std::ifstream	infile(filename);
-	std::ofstream	stm(filename + ".replace");
-	std::string	line;
-
-	if (!stm)
-		std::cout << "Le fichier n'existe pas." << std::endl;
-	while (std::getline(infile, line)) {
-
-		size_t pos = 0;
-		while (( pos = line.find(s1, pos)) != std::string::npos) {
-
-
-
-
-
-
-			pos += s1.length();
-		}		
-	}	
-}
-
 int	main(int argc, char **argv)
 {
-	if (argc == 4)
-		SedIsForLosers(argv[1], argv[2], argv[3]);
-	else
-		std::cout << "respect input rules" << std::endl;
-	return (0);
+	
+ 	return (0);
 }
 
 
